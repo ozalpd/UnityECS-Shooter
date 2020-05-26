c966f647fe74af8abd1f33261c0c1b5ea67a7db2
Özalp Döndüren <ozalp@donduren.com>
2020-05-26 21:36:41 +0300

    Added red blood cells into scene and BulletMoveSystem to fire droplet bullets

---
+++ Assets/Scripts/BulletMoveSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+
+public class BulletMoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float dt = Time.DeltaTime;
+        var jobHandle = Entities.WithName("BulletMoveSystem")
+                                        //this is a parallel processing iteration
+                                        .ForEach((ref PhysicsVelocity physics,
+                                                  ref Translation position,
+                                                  ref Rotation rotation,
+                                                  ref BulletData bulletData) =>
+                                        {
+                                            physics.Angular = float3.zero;
+                                            physics.Linear += dt * bulletData.speed * math.forward(rotation.Value);
+                                        })
+                                        .Schedule(inputDeps);
+        jobHandle.Complete();
+        return jobHandle;
+    }
+}
--- Assets/Scripts/ECSManager.cs
+++ Assets/Scripts/ECSManager.cs
@@ -7,22 +7,41 @@ using Unity.Transforms;
 public class ECSManager : MonoBehaviour
 {
     public static EntityManager manager;
+    public GameObject player;
+
     public GameObject virusPrefab;
     public int numVirus = 500;
+
+    public GameObject redBloodCellPrefab;
+    public int numBloodCell = 500;
+
+    public GameObject bulletPrefab;
+    public int numBullets = 10;
+    private Entity bulletEntity;
 
     BlobAssetStore store;
 
 
-    void Start()
+    private void Start()
     {
         store = new BlobAssetStore();
         manager = World.DefaultGameObjectInjectionWorld.EntityManager;
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, store);
-        Entity virus = GameObjectConversionUtility.ConvertGameObjectHierarchy(virusPrefab, settings);
 
-        for (int i = 0; i < numVirus; i++)
+        Entity virus = GameObjectConversionUtility.ConvertGameObjectHierarchy(virusPrefab, settings);
+        Instaniate(virus, numVirus);
+
+        Entity redCell = GameObjectConversionUtility.ConvertGameObjectHierarchy(redBloodCellPrefab, settings);
+        Instaniate(redCell, numBloodCell);
+
+        bulletEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(bulletPrefab, settings);
+    }
+
+    private void Instaniate(Entity entity, int numOfInstance)
+    {
+        for (int i = 0; i < numOfInstance; i++)
         {
-            var instance = manager.Instantiate(virus);
+            var instance = manager.Instantiate(entity);
             float3 position = GetRandomFloat3(-50, 50);
             manager.SetComponentData(instance, new Translation() { Value = position });
             manager.SetComponentData(instance, new FloatData()
@@ -32,11 +51,31 @@ public class ECSManager : MonoBehaviour
         }
     }
 
-    float3 GetRandomFloat3(float min, float max)
+    private float3 GetRandomFloat3(float min, float max)
     {
         return new float3(UnityEngine.Random.Range(min, max),
                           UnityEngine.Random.Range(min, max),
                           UnityEngine.Random.Range(min, max));
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        for (int i = 0; i < numBullets; i++)
+        {
+            var instance = manager.Instantiate(bulletEntity);
+            var startPos = player.transform.position + UnityEngine.Random.insideUnitSphere * 2;
+            manager.SetComponentData(instance, new Translation() { Value = startPos });
+            manager.SetComponentData(instance, new Rotation() { Value = player.transform.rotation });
+        }
     }
 
     private void OnDestroy()
